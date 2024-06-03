c418354a06e78b8c1f2ad19ce3bcf647a08df350
Heitor Toledo Lassarote de Paula <heitortoledo@hotmail.com>
2024-06-03 13:30:10 -0300

    Add a lexer for the lambda calculus
    Problem: We want to make a parser for the lambda calculus, but first we
    need a lexer.
    
    Solution: Implement a lexer for Church.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,82 @@
-use std::{collections::HashMap, rc::Rc};
+use std::{collections::HashMap, iter::Peekable, rc::Rc, str::Chars};
+
+#[derive(Debug)]
+enum Tok {
+    Lam,
+    Dot,
+    LPar,
+    RPar,
+    Id(String),
+}
+
+struct Lexer<'a> {
+    peekable: Peekable<Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Self {
+        Lexer {
+            peekable: input.chars().peekable(),
+        }
+    }
+
+    fn peek(&mut self) -> Option<char> {
+        self.peekable.peek().copied()
+    }
+
+    fn skip_whitespace(&mut self) -> () {
+        while self.peek().map_or(false, char::is_whitespace) {
+            self.peekable.next();
+        }
+    }
+
+    fn is_id_char(c: char, idx: usize) -> bool {
+        if idx == 0 {
+            c.is_alphabetic() && c != 'λ'
+        } else {
+            c.is_alphanumeric() && c != 'λ'
+        }
+    }
+
+    fn id(&mut self) -> String {
+        let mut id = String::new();
+        while let Some(c) = self.peek() {
+            if Self::is_id_char(c, id.len()) {
+                id.push(c);
+                self.peekable.next();
+            } else {
+                break;
+            }
+        }
+        id
+    }
+
+    fn eat(&mut self, tok: Tok) -> Option<Tok> {
+        self.peekable.next()?;
+        Some(tok)
+    }
+
+    fn advance(&mut self) -> Option<Tok> {
+        let c = self.peek()?;
+        let result = match c {
+            '\\' | 'λ' => self.eat(Tok::Lam),
+            '.' => self.eat(Tok::Dot),
+            '(' => self.eat(Tok::LPar),
+            ')' => self.eat(Tok::RPar),
+            _ => Some(Tok::Id(self.id())),
+        };
+        self.skip_whitespace();
+        result
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Tok;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.advance()
+    }
+}
 
 #[derive(Debug)]
 enum Church {
@@ -131,6 +209,10 @@ fn main() {
     );
 
     println!("Input:  {:?}", exp);
+    let input = r"(λx. λy. x) a b";
+    println!("String: {:?}", input);
+    let tokens: Vec<Tok> = Lexer::new(input).collect();
+    println!("Tokens: {:?}", tokens);
     let result = eval(rename(exp));
     println!("Result: {:?}", result);
 }
